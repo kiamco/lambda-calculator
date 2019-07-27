0d2e8a48ca040c5fdf9f43bef2e8ba14f8bf5e35
kiamco <kiamcoo@gmail.com>
2019-07-27 00:09:26 -0700

    rendered htmls components

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
-
+import NumberButton from "./NumberButton"
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+
+const Numbers = (props) => {
   // STEP 2 - add the imported data to state
+  console.log(props)
+  const [number, setNumber] = useState(props.numbers);
   return (
-    <div>
+    <div className="numbers">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {
+       number.map(el => {
+        return <NumberButton calcNumber={el} />
+       })
+      }
     </div>
   );
 };
+
+export default Numbers;
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,22 @@
 import React from "react";
 
 //import any components needed
-
+import OperatorButton from "./OperatorButton"
 //Import your array data to from the provided data file
 
-const Operators = () => {
+const Operators = (props) => {
   // STEP 2 - add the imported data to state
+  console.log(props)
   return (
-    <div>
+    <div className="operators">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {
+         props.operator.map(el => <OperatorButton calcOp={el} />)
+       }
     </div>
   );
 };
+
+export default Operators
